0d673c70061009baf0eb2111f239d4a40236bd56
Jovany Leandro G.C bit4bit@riseup.net <bit4bit@riseup.net>
2023-07-06 12:44:14 -0500

    chore: ert

--- test/clockifun-tests.el
+++ test/clockifun-tests.el
@@ -1,2 +1,19 @@
-(ert-deftest require-mode ()
-  (require 'clockifun))
+;;; clockifun-tests.el --- Tests -*- lexical-binding: t -*-
+
+;;; Commentary:
+
+;; Clockifun tests
+
+;;; Code:
+
+(require 'ert)
+
+(ert-deftest clockifun-test-require-mode-pass ()
+  (let ((test (make-ert-test :body (lambda () (require 'clockifun)))))
+    (let ((result (ert-run-test test)))
+      (should (ert-test-passed-p result)))))
+
+
+(provide 'clockifun-tests)
+
+;;; clockifun-tests.el ends here
