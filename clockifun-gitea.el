f8bdbac7ab05bbd4b03c687701adc39af90c7864
Jovany Leandro G.C bit4bit@riseup.net <bit4bit@riseup.net>
2023-07-07 17:20:47 -0500

    feat: add gitea stuff

--- clockifun-gitea.el
+++ clockifun-gitea.el
@@ -8,6 +8,7 @@
 ;;; Code:
 
 (defconst ORG-ISSUE-ID "CLOCKIFUN-GITEA-ISSUE-ID")
+(defconst ORG-REPOSITORY "CLOCKIFUN-GITEA-REPOSITORY")
 
 (defun clockifun-gitea--extract-issue-id (title)
   (when (stringp title)
@@ -19,13 +20,16 @@
 
 (defun clockifun-gitea--org-entry-at-endpoint->repository ()
   (save-restriction
-    (let ((element (org-entry-get nil "CLOCKIFUN-GITEA-REPOSITORY" t)))
+    (let ((element (org-entry-get nil ORG-REPOSITORY t)))
       element)))
 
 (defun clockifun-gitea--org-entry-at-endpoint->issue-id ()
   (save-restriction
     (let ((element (org-entry-get nil ORG-ISSUE-ID t)))
       element)))
+
+(defun clockifun-gitea--repository->org-entry-at-endpoint (repo)
+  (org-entry-put nil ORG-REPOSITORY repo))
 
 (defun clockifun-gitea--issue-id->org-entry-at-endpoint (id)
   (org-entry-put nil ORG-ISSUE-ID id))
@@ -40,14 +44,56 @@
         maybe-issue-id
       (clockifun-gitea--ask-user-for-issue))))
 
+(defun clockifun-gitea--parse-gitea-issues-data (data)
+  (mapcar (lambda (issue)
+            (cons (number-to-string (alist-get 'id issue))
+                  (alist-get 'title issue)))
+          (json-read-from-string data)))
+
 (defun clockifun-gitea--clock-in ()
   (let ((issue-id (clockifun-gitea--get-issue-id))
+        (repository (clockifun-gitea--org-entry-at-endpoint->repository))
         )
-    (unless issue-id (user-error "Fails to get a issue id"))
+    (unless repository (user-error "Fails to get a REPOSITORY"))
+    (unless issue-id (user-error "Fails to get a ISSUE ID"))
+    
     (clockifun-gitea--issue-id->org-entry-at-endpoint issue-id)
     ))
 
 (defun clockifun-gitea--clock-out ())
+
+(defun clockifun-gitea--gitea-auth-token (host)
+  (let ((found (nth 0 (auth-source-search :host host))))
+    (unless found (error (format "not found auth token for host %s" host)))
+    (funcall (plist-get found :secret))))
+
+(defun clockifun-gitea--gitea-call (host method resource &optional body)
+  "Do call to gitea HOST using http METHOD to RESOURCE with BODY."
+  (let* ((secret (clockifun-gitea--gitea-auth-token host))
+         (url-request-method method)
+         (url-request-extra-headers
+          (list '("Content-Type" . "application/json")
+                (cons "Authorization"  (concat "token " secret))))
+         (service (if (listp resource) (string-join resource "/") resource))
+         (url-request-data (if (listp body)
+                               (replace-regexp-in-string "[^[:ascii:]]" "?"
+                                                         (json-encode-list body)) nil))
+         )
+    
+    (with-current-buffer
+        (url-retrieve-synchronously (concat "https://" host service))
+      (goto-char (point-min))
+      (search-forward-regexp "\n\n")
+      (buffer-substring (point) (point-max))
+      )))
+
+(defun clockifun-gitea--gitea-issues (host username repo)
+  (let* ((token (clockifun-gitea--gitea-auth-token host))
+         (data (clockifun-gitea--gitea-call
+                host
+                "GET"
+                (concat  "/api/v1/repos/" username "/" repo "/issues"))))
+    (clockifun-gitea--parse-gitea-issues-data data)))
 
 (defun clockifun-stopwatcher-gitea ()
   (list
--- test/clockifun-gitea-tests.el
+++ test/clockifun-gitea-tests.el
@@ -36,6 +36,14 @@
             (clockifun-gitea--org-entry-at-endpoint->repository)))))
 
 
+(ert-deftest clockifun-gitea-test-clock-in-save-repository-in-org-entry-property ()
+  (should (string=
+           "* DEMO\n  :PROPERTIES:\n  :CLOCKIFUN-GITEA-REPOSITORY: DEMO\n  :END:\n"
+           (org-test-with-temp-text
+            "* DEMO"
+            (clockifun-gitea--repository->org-entry-at-endpoint "DEMO")
+            (buffer-string)))))
+
 ;; al iniciar reloj
 ;; - consultar issues : externo
 ;; - [x] almacenar property en entrada org
@@ -48,6 +56,32 @@
             (clockifun-gitea--issue-id->org-entry-at-endpoint "123")
             (buffer-string)))))
 
+;; INTEGRATION
+
+(ert-deftest clockifun-gitea-test-parse-gitea-issues ()
+  ;;https://try.gitea.io/api/swagger#/issue/issueListIssues
+  (let ((remote-data "[{\"id\": 123, \"title\":\"DEMO\"}, {\"id\": 456, \"title\": \"TEST\"}]"))
+    (should (equal
+             '(("123" . "DEMO")
+               ("456" . "TEST"))
+             (clockifun-gitea--parse-gitea-issues-data remote-data)
+             ))))
+
+;; ACCEPTANCE
+
+(ert-deftest clockifun-gitea-test-clock-in-when-not-have-repository-raise-error ()
+  (with-mock
+   (stub clockifun-gitea--ask-user-for-issue => nil)
+
+   ;; uuum? repository error?
+   (should-error
+    (with-stopwatcher
+     (symbol-function 'clockifun-stopwatcher-gitea)
+     (org-test-with-temp-text
+      "* DEMO"
+      (unwind-protect (org-clock-in) (org-clock-out))
+      )) :type 'user-error)))
+
 (ert-deftest clockifun-gitea-test-clock-in-when-not-have-ask-user ()
   (with-mock
    (mock (clockifun-gitea--ask-user-for-issue) => "123")
@@ -56,6 +90,7 @@
     (symbol-function 'clockifun-stopwatcher-gitea)
     (org-test-with-temp-text
      "* DEMO"
+     (clockifun-gitea--repository->org-entry-at-endpoint "DEMO")
      (org-clock-in)
      (org-clock-out)))))
 
@@ -68,9 +103,22 @@
      (symbol-function 'clockifun-stopwatcher-gitea)
      (org-test-with-temp-text
       "* DEMO"
+      (clockifun-gitea--repository->org-entry-at-endpoint "DEMO")
       (unwind-protect (org-clock-in) (org-clock-out))
       )) :type 'user-error)))
 
+(ert-deftest clockifun-gitea-test-gitea-issues ()
+  (with-mock
+   (mock (clockifun-gitea--gitea-auth-token "gitea.test.org") => "demo")
+   (mock (clockifun-gitea--gitea-call "gitea.test.org" "GET" "/api/v1/repos/bit4bit/prueba/issues") => "[{\"id\": 1, \"title\": \"demo\"}, {\"id\": 2, \"title\": \"test\"}]")
+
+   (clockifun-gitea--gitea-issues "gitea.test.org" "bit4bit" "prueba")))
+
+;; MANUAL
+
+;; ~/.authinfo configurar
+;; (clockifun-gitea--gitea-call "gitea.server.org" "GET" "/api/v1/repos/bit4bit/prueba/issues")
+;; (clockifun-gitea--gitea-issues)
 (provide 'clockifun-gitea)
 
 ;;; clockifun-gitea-tests.el ends here
